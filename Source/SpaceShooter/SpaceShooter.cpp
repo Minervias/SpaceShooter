a8ba263f190e70ac52541e784851f3cc475cd899
Minervias <kylewsipe@gmail.com>
2018-09-23 18:47:43 -0500

    Initial

---
+++ Source/SpaceShooter/SpaceShooter.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceShooter, "SpaceShooter" );
+
+DEFINE_LOG_CATEGORY(LogSpaceShooter)
+ 
